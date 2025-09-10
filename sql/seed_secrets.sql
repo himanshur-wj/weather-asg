6383e6616b573a92b859fc938066ae9f8b4945f3
Himanshu Ranjan <himanshur@winjit.com>
2025-09-10 14:58:25 +0530

    update

--- sql/seed_secrets.sql
+++
@@ -1,6 +0,0 @@
-USE weather_app;
-
--- Replace placeholders with your real tokens/keys locally (do NOT commit secrets).
-INSERT INTO secrets (provider, key_name, key_value) VALUES
-('mapbox','access_token','REPLACE_WITH_YOUR_MAPBOX_TOKEN'),
-('openweathermap','api_key','REPLACE_WITH_YOUR_OWM_KEY');
